74ccd54989dcd4c5d8e7bfd7d0391547e14da96e
christhodo <102981895+christhodo@users.noreply.github.com>
2022-08-17 11:16:31 -0700

    add initial file structure

--- apps/clone/src/app/app.module.ts
+++ apps/clone/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CoursesListComponent } from './courses/courses-list/courses-list.component';
+import { CourseDetailsComponent } from './courses/course-details/course-details.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    HomeComponent,
+    CoursesComponent,
+    CoursesListComponent,
+    CourseDetailsComponent,
+  ],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/clone/src/app/courses/courses-list/courses-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'clone-app-courses-list',
+  templateUrl: './courses-list.component.html',
+  styleUrls: ['./courses-list.component.scss'],
+})
+export class CoursesListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
